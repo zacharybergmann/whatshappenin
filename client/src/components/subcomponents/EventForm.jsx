355245beb9fa3875e97bf33ea781593458b4e6e1
Eric Periou <eperiou@yahoo.com>
2017-02-23 22:01:39 -0600

    (feature) add event form (style) aligns items for better visual appeal on main page

---
+++ client/src/components/subcomponents/EventForm.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+// import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const style = {
+  marginLeft: 20,
+};
+
+const EventForm = () => {
+  let Form = {
+    name: '',
+    location: '',
+    time: '',
+    friends: '',
+  };
+
+  const logForm = function (event) {
+    console(event, user);
+  };
+  return (
+
+    <form onSubmit={logForm} >
+      <div>
+        <TextField
+          name="name"
+          type="name"
+          value={Form.name}
+          hintText="what goes in here"
+          style={style}
+        />
+      </div>
+      <div>
+        <TextField
+          name="location"
+          type="location"
+          value={Form.location}
+          hintText="Where tha party at?!"
+          style={style}
+        />
+      </div>
+      <div>
+        <TextField
+          name="time"
+          type="time" value={Form.time}
+          hintText="When I gotta be there?" style={style}
+        />
+      </div>
+      <div>
+        <TextField
+          name="friends"
+          type="friends"
+          value={Form.friends}
+          hintText="Can I bring somebody?" style={style}
+        />
+      </div>
+      <div>
+        <RaisedButton type="submit" label="make an Event" />
+      </div>
+    </form>
+  );
+};
+
+export default EventForm;
