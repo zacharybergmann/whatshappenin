56bd8a7decb17f0170442b0e5d919f0d7ce52ccf
Eric Periou <eperiou@yahoo.com>
2017-02-28 15:14:55 -0600

    (feature)Add ability to get current location and set the event to it, condenses repetative code into functional component

--- client/src/components/subcomponents/EventForm.jsx
+++ client/src/components/subcomponents/EventForm.jsx
@@ -17,114 +17,116 @@ const EventForm = ({
   handleDate,
   location,
   errors,
-}) => {
-  return (
-    <form action="/" onSubmit={processForm}>
-      <div>
-        <TextField
-          name="title"
-          type="title"
-          hintText="What's the party called?"
-          style={style}
-          value={eventDetails.title}
-          onChange={eveChange}
-          errorText={errors.title}
-        />
-      </div>
-      <div>
-        <TextField
-          id="locationslot"
-          name="location"
-          type="location"
-          hintText="Where tha party at?!"
-          style={style}
-          value={`${location.address}\
-          longitude: ${location.longitude},\
-          latitude: ${location.latitude}`}
-          onChange={eveChange}
-          errorText={errors.location}
-        />
-      </div>
-      <div>
-        <TimePicker
-          name="eventTime"
-          type="eventTime"
-          hintText="12hr Format"
-          style={style}
-          onChange={handleTime}
-          value={eventDetails.eventTimeObj}
-          errorText={errors.eventTime}
-        />
-      </div>
-      <div>
-        <DatePicker
-          type="eventDate"
-          hintText="Date"
-          name="eventDate"
-          style={style}
-          onChange={handleDate}
-          value={eventDetails.eventDateObj}
-          errorText={errors.eventDate}
-        />
-      </div>
-      <div>
-        <TextField
-          name="picLink"
-          type="picLink"
-          hintText="Got an image link to share?"
-          style={style}
-          value={eventDetails.picLink}
-          onChange={eveChange}
-          errorText={errors.picLink}
-        />
-      </div>
-      <div>
-        <TextField
-          name="busLink"
-          type="busLink"
-          hintText="Promote your business' website"
-          style={style}
-          value={eventDetails.busLink}
-          onChange={eveChange}
-          errorText={errors.busLink}
-        />
-      </div>
-      <div>
-        <TextField
-          name="businessName"
-          type="businessName"
-          hintText="What's your business' name?"
-          style={style}
-          value={eventDetails.businessName}
-          onChange={eveChange}
-        />
-      </div>
-      <div>
-        <TextField
-          name="tags"
-          type="tags"
-          hintText="tags"
-          style={style}
-          value={eventDetails.tags}
-          onChange={eveChange}
-        />
-      </div>
-      <div>
-        <TextField
-          name="description"
-          type="description"
-          hintText="Describe your sweet event"
-          style={style}
-          value={eventDetails.description}
-          onChange={eveChange}
-        />
-      </div>
-      <div>
-        <RaisedButton type="submit" label="make an Event" />
-      </div>
-    </form>
-  );
-};
+  closeDrawer,
+}) =>
+  <form action="/" onSubmit={processForm}>
+    <div>
+      <TextField
+        name="title"
+        type="title"
+        hintText="What's the party called?"
+        style={style}
+        value={eventDetails.title}
+        onChange={eveChange}
+        errorText={errors.title}
+      />
+    </div>
+    <div>
+      <TextField
+        multiLine
+        id="locationslot"
+        name="location"
+        type="location"
+        hintText="Where tha party at?!"
+        style={style}
+        value={`${location.address}\
+        longitude: ${location.longitude},\
+        latitude: ${location.latitude}`}
+        onChange={eveChange}
+        errorText={errors.location}
+      />
+    </div>
+    <div>
+      <TimePicker
+        name="eventTime"
+        type="eventTime"
+        hintText="12hr Format"
+        style={style}
+        onChange={handleTime}
+        value={eventDetails.eventTimeObj}
+        errorText={errors.eventTime}
+      />
+    </div>
+    <div>
+      <DatePicker
+        type="eventDate"
+        hintText="Date"
+        name="eventDate"
+        style={style}
+        onChange={handleDate}
+        value={eventDetails.eventDateObj}
+        errorText={errors.eventDate}
+      />
+    </div>
+    <div>
+      <TextField
+        name="picLink"
+        type="picLink"
+        hintText="Got an image link to share?"
+        style={style}
+        value={eventDetails.picLink}
+        onChange={eveChange}
+        errorText={errors.picLink}
+      />
+    </div>
+    <div>
+      <TextField
+        name="busLink"
+        type="busLink"
+        hintText="Promote your business' website"
+        style={style}
+        value={eventDetails.busLink}
+        onChange={eveChange}
+        errorText={errors.busLink}
+      />
+    </div>
+    <div>
+      <TextField
+        name="businessName"
+        type="businessName"
+        hintText="What's your business' name?"
+        style={style}
+        value={eventDetails.businessName}
+        onChange={eveChange}
+      />
+    </div>
+    <div>
+      <TextField
+        multiLine
+        name="tags"
+        type="tags"
+        hintText="tags"
+        style={style}
+        value={eventDetails.tags}
+        onChange={eveChange}
+      />
+    </div>
+    <div>
+      <TextField
+        multiLine
+        name="description"
+        type="description"
+        hintText="Describe your sweet event"
+        style={style}
+        value={eventDetails.description}
+        onChange={eveChange}
+      />
+    </div>
+    <div>
+      <RaisedButton onClick={closeDrawer} type="submit" label="Post Event" />
+    </div>
+  </form>;
+
 
 EventForm.propTypes = {
   eventDetails: React.PropTypes.object.isRequired,
@@ -132,7 +134,9 @@ EventForm.propTypes = {
   processForm: React.PropTypes.func.isRequired,
   handleTime: React.PropTypes.func.isRequired,
   handleDate: React.PropTypes.func.isRequired,
+  closeDrawer: React.PropTypes.func.isRequired,
   location: React.PropTypes.object.isRequired,
+  errors: React.PropTypes.object.isRequired,
 };
 
 export default EventForm;
